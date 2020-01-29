5340081a56e08f380b5eafb250e096f4da1699c5
John Nolette <jnolette@transparent.com>
2020-01-29 18:16:27 -0500

    Finishing practice for num, euler's problem.

--- practice/num.rs
+++ practice/num.rs
@@ -1,3 +1,10 @@
 fn main() {
-
+  let max: u32 = 1000;
+  let mut sum: u32 = 0;
+  for n in 1..max {
+    if n % 3 == 0 || n % 5 == 0 {
+      sum += n;
+    }
+  }
+  println!("sum: {}", sum);
 }
--- practice/string.rs
+++ practice/string.rs
@@ -2,4 +2,23 @@ fn main() {
   // reference to a string located in read-only memory
   let p: &'static str = "the quick brown fox jumps over the lazy dog";
   println!("Pangram: {}", p);
+  // iterate over words
+  // - can alternatively use `split_whitespace`
+  for s in p.split(' ') {
+    println!("> {}", s);
+  }
+  // mutable string
+  let mut s = "";
+  s += 'a';
+  println!(s);
+  // raw string with quotes
+  let quotes = r#"Then I screamed, "Hello World""#;
+  println!("{}", quotes);
+  // multiline strings
+  let multi = "
+  this
+  is
+  a
+  test
+  ";
 }
